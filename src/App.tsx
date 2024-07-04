c7e33246a64c7933732873edba4b2b78ab2733ac
kuloneko <zzj32898@hotmail.com>
2024-07-04 17:13:03 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import "./App.css";
+import { TonConnectButton } from "@tonconnect/ui-react";
+import { useMainContract } from "./hooks/useMainContract";
+import { useTonConnect } from "./hooks/useTonConnect";
+
+function App() {
+  const {
+    contract_address,
+    counter_value,
+    recent_sender,
+    owner_address,
+    owner_balance,
+    sendIncrement,
+  } = useMainContract();
+
+  const { connected } = useTonConnect();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TonConnectButton />
+      </header>
+      <main className="App-main">
+        {connected && (
+          <button className="Increment-button" onClick={sendIncrement}>
+            Increment
+          </button>
+        )}
+        <div className="Card">
+          <h2>Our Contract Address</h2>
+          <p className="Hint">{contract_address?.slice(0, 30) + "..."}</p>
+          <h2>Our Contract Balance</h2>
+          <p className="Hint">{owner_balance ?? "等等啊我找找..."}</p>
+        </div>
+
+        <div className="Card">
+          <h2>Counter Value</h2>
+          <p className="Hint">{counter_value ?? "不要催在找了..."}</p>
+        </div>
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { manifestUrl } from '../public/manifest_url.json';
+
+// this manifest is used temporarily for development purposes
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>,
+);
